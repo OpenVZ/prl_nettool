7aa19b5991bb7234b6579280c250715643b73455
Alexander Pisarev <apisarev@virtuozzo.com>
2016-02-19 17:56:27 +0300

    Fix set ip for VM with NM and interface method link-local. #PSBM-44221

--- Linux/scripts/nm-set_ip.sh
+++ Linux/scripts/nm-set_ip.sh
@@ -90,8 +90,9 @@ function set_ip()
 {
 	local ip_mask ip mask
 	local new_ips
-	local ifv4_num=0
-	local ifv6_num=0
+	local plusv4=""
+	local plusv6=""
+	local errors=0
 
 	new_ips="${IP_MASKS}"
 	for ip_mask in ${new_ips}; do
@@ -102,10 +103,14 @@ function set_ip()
 		fi
 	done
 
+	call_nmcli c modify $uuid ipv4.method auto ||
+		return $?
+	call_nmcli c modify $uuid ipv6.method auto ||
+		return $?
+
 	new_ips="${IP_MASKS}"
 	for ip_mask in ${new_ips}; do
 		if ! is_ipv6 ${ip_mask}; then
-			ifv4_num=$((ifv4_num + 1))
 			if echo ${ip_mask} | grep -q '/' ; then
 				mask=`mask2cidr ${ip_mask##*/}`
 			else
@@ -114,51 +119,42 @@ function set_ip()
 			ip=${ip_mask%%/*}
 
 			if [ "$mask" -ge 0 ]; then
-				if [ $ifv4_num -eq 1 ]; then
-					call_nmcli c modify $uuid ipv4.address $ip/$mask
-				else
-					call_nmcli c modify $uuid +ipv4.address $ip/$mask
-				fi
+				call_nmcli c modify $uuid ${plusv4}ipv4.address $ip/$mask
+				[ $? -ne 0 ] && errors=$((errors + 1))
+				plusv4="+"
 			fi
 		else
-			ifv6_num=$((ifv6_num + 1))
-			if [ $ifv6_num -eq 1 ]; then
-				call_nmcli c modify $uuid ipv6.address $ip_mask
-			else
-				call_nmcli c modify $uuid +ipv6.address $ip_mask
-			fi
+			call_nmcli c modify $uuid ${plusv6}ipv6.address $ip_mask
+			[ $? -ne 0 ] && errors=$((errors + 1))
+			plusv6="+"
 		fi
 	done
 
 	if [ $IP4_COUNT -eq 0 ]; then
-		if [ $USE_DHCPV4 -eq 1 ]; then
-			call_nmcli c modify $uuid ipv4.method auto ||
-				return $?
-		else
+		call_nmcli c modify $uuid ipv4.gateway ""
+		call_nmcli c modify $uuid ipv4.address ""
+		if [ $USE_DHCPV4 -eq 0 ]; then
 			call_nmcli c modify $uuid ipv4.method link-local ||
 				return $?
 		fi
-		call_nmcli c modify $uuid ipv4.gateway ""
-		call_nmcli c modify $uuid ipv4.address ""
 	else
 		call_nmcli c modify $uuid ipv4.method manual ||
 			return $?
 	fi
 
 	if [ $IP6_COUNT -eq 0 ]; then
-		if [ $USE_DHCPV6 -eq 1 ]; then
-			call_nmcli c modify $uuid ipv6.method auto ||
-				return $?
-		else
+		call_nmcli c modify $uuid ipv6.gateway ""
+		call_nmcli c modify $uuid ipv6.address ""
+		if [ $USE_DHCPV6 -eq 0 ]; then
 			call_nmcli c modify $uuid ipv6.method link-local ||
 				return $?
 		fi
-		call_nmcli c modify $uuid ipv6.gateway ""
-		call_nmcli c modify $uuid ipv6.address ""
 	else
 		call_nmcli c modify $uuid ipv6.method manual ||
 			return $?
 	fi
+
+	return $errors
 }
 
 set_ip
