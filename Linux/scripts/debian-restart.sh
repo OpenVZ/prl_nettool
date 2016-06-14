19934c9a8611fee603f53f8443ea517c0a3e4284
Alexander Pisarev <apisarev@virtuozzo.com>
2016-06-14 16:39:25 +0300

    Add restart for separate interface to debian scripts. #PSBM-24502
    Before fix on any change in network configuration lead to whole network restart.
    Such approach was redundant, so decided to restart only affected interfaces.
    Error at interface restart lead to a whole network restart, cause it may successfully end in some cases.
    This patch do it for debian based distributions.

--- Linux/scripts/debian-restart.sh
+++ Linux/scripts/debian-restart.sh
@@ -18,8 +18,32 @@ else
 	exit 1
 fi
 
+function restart_conf()
+{
+	/sbin/ifdown $1 &&
+		/sbin/ifup $1
+}
+
+function restart_iface()
+{
+	local iface=$1
+
+	restart_conf $iface || return $?
+	
+	conf_suffix=(`cat /etc/network/interfaces | grep "^\s*auto $iface:.*$" | sed "s@\s*auto $iface:\(\d*\)\s*@\1@"`)
+	for suffix in ${conf_suffix[@]}
+	do
+		restart_conf "$iface:$suffix" || return $?
+	done
+}
+
 function restart()
 {
+	if [ $# -ne 0 ]; then
+		restart_iface $1 &&
+			return 0
+	fi
+
 	if [ -x /usr/sbin/service ]; then
 		# Ubuntu 12.04 contains only start in upstart config
 		/etc/init.d/networking stop > /dev/null 2>&1 </dev/null
@@ -37,6 +61,13 @@ function restart()
 
 
 function restart_nm() {
+	type nmctl > /dev/null 2>&1
+	if [ $? -eq 0 -a $# -ne 0 ]; then
+		nmcli device disconnect $1 &&
+			nmcli device connect $1 &&
+			return 0
+	fi
+
 	# In ubuntu 14.04 Desktop or based distros like Linux Mint
 	# network-manager is restarted and watched by initctl
 	type initctl > /dev/null 2>&1 &&
@@ -47,9 +78,9 @@ function restart_nm() {
 }
 
 if [ -f $NWSYSTEMCONF -o -f $NMCONFFILE ]; then
-	restart_nm
+	restart_nm $1
 else
-	restart
+	restart $1
 fi
 
 exit 0
--- Linux/scripts/debian-set_ip.sh
+++ Linux/scripts/debian-set_ip.sh
@@ -337,7 +337,7 @@ else
 	set_ip
 fi
 
-$path/debian-restart.sh
+$path/debian-restart.sh ${ETH_DEV}
 
 exit 0
 # end of script
